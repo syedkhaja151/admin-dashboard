61e3096034540264160e0d369d506b2ca09b67ac
khaja <syedkhajahassan151@gmail.com>
2025-03-13 16:53:28 +0530

    created files

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,44 @@
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import React from 'react';
+ import {Link} from "react-router-dom";
+import {SiShopware} from "react-icons/si";
+ import { MdOutlineCancel } from "react-icons/md";
+import { links } from '../data/Data/dummy';
+
+ 
+
+const SideBar = () => {
+  const activeMenu =  true ;
+  return (
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+      {activeMenu &&(<>
+      <div className='flex justify-between item-center'>
+        <Link to='/' onClick={() => {}}
+        className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+        <SiShopware /><span>Shoppy</span>
+        </Link>
+        <TooltipComponent content="Menu" position="BottomCenter">
+              <button
+                type="button"
+                // onClick={() => setActiveMenu(!activeMenu)}
+                // style={{ color: currentColor }}
+                // className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+              >
+                <MdOutlineCancel />
+              </button>
+            </TooltipComponent>
+      </div>
+      <div className='mt-10'>
+        {links.map((items) =>(
+          <div className='text-gray-400 m-3 mt-4 uppercase'>
+            {items.title}
+          </div>
+        ))}
+
+      </div>
+      </>)}
+    </div>
+  )
+}
+
+export default SideBar
